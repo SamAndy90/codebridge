35a5236e1f89d71fd0561d2a08cf24ac3da11b0a
SamAndy90 <stadnyk.andy@gmail.com>
2023-01-17 12:59:44 +0200

    third commit

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -93,7 +93,7 @@ export function HomePage() {
             {filteredArticles.map((i) => {
                 return (
                   <Grid item xs={4} key={i.id}>
-                    <Link to={"/codebridge-test-app/article/"} >
+                    <Link to={"/codebridge/article/"} >
                     <ArticleCard
                       imageSrc={i.imageUrl}
                       title={
